b0dc8748e95ad90ff47b77be0b86fcb496d9c229
GF-Cris <cristina.gallego.fabrega@gmail.com>
2015-10-25 18:49:47 +0100

    R Programming Assigment 2. Coursera
    Compute the inverse of a given matrix object if is not already in the
    cache,  if so, returns the existing value from the cache.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,41 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This function creates a special "matrix" object that can cache its inverse.
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(Matrix = matrix()) {
+  inverse <- NULL 
+  
+  SetMatrix <- function(NewMatrix) {
+    Matrix <<- NewMatrix    # Set the value of the matrix
+    inverse <<- NULL        # Clear the existing inverse from the cache
+  }
+  GetMatrix <- function(){
+    MatrixValues   #Get the value of the matrix
+  } 
+  SetInverse<- function(solve){
+    inverse <<- NewInverse   #Used by getinverse() when there is no inverse
+  } 
+  GetInverse <- function(){
+    inverse  # Get the inverse of the matrix.
+  }
+  list(set = SetMatrix,  # Return a list with the above four functions
+       get = GetMatrix,
+       setinverse = SetInverse,
+       getinverse = GetInverse)
 }
 
 
-## Write a short comment describing this function
+## This function computes the inverse of a given matrix object if is not already in the cache,
+#  if so, returns the existing value from the cache.
 
-cacheSolve <- function(x, ...) {
+
+cacheSolve <- function(Matrix, ...) {
         ## Return a matrix that is the inverse of 'x'
+  inverse <- Matrix$GetInverse()
+  if(!is.null(inverse)) {
+    message("getting cached data")
+    return(inverse)
+  }
+  data <- Matrix$GetMatrix()
+  inverse <- solve(data, ...)
+  Matrix$SetInverse(inverse)
+  inverse
 }
